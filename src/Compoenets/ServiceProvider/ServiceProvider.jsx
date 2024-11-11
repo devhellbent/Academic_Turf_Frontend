905a382741155b5b72cff0a67a4e006285d6f82c
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-11-11 12:38:52 +0530

    push

--- src/Compoenets/ServiceProvider/ServiceProvider.jsx
+++ src/Compoenets/ServiceProvider/ServiceProvider.jsx
@@ -31,7 +31,7 @@ export default function Component() {
 
   const fetchUsers = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/users");
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/users`);
       const data = await response.json();
       setUsers(data);
       setFilteredUsers(data);
